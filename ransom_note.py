95778736797be1141d9bf7c56327982eaaf8e104
Xavi Anderhub <xavihub@gmail.com>
2020-08-29 02:00:11 -0500

    completed ransom note

---
+++ ransom_note.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the checkMagazine function below.
+def checkMagazine(magazine, note):
+    note_words = {}
+
+    # Add all words in note to dict
+    for word in note:
+        if word in note_words:
+            note_words[word] += 1
+        else:
+            note_words[word] = 1
+
+    for word in magazine:
+        if word in note_words:
+            if note_words[word] != 0: note_words[word] -= 1
+    
+    for key in note_words:
+        if note_words[key] != 0:
+            print("No")
+            return
+
+    print('Yes')
+
+magazine = ['hi', 'there', 'bob']
+note = ['hi', 'there']
+print(checkMagazine(magazine, note))
